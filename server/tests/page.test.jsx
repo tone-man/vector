b84896d6579e001f40ab7ae10d35a790ea31f5d2
tone-man <thetoneman@outlook.com>
2024-05-14 17:03:27 -0400

    Jest Hello World

---
+++ server/tests/page.test.jsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '../src/app/page'
+ 
+describe('Page', () => {
+  it('renders hello world', () => {
+    render(<Page />)
+ 
+    const heading = screen.getByRole('heading', { level: 1 });
+ 
+    expect(heading).toBeInTheDocument();
+  })
+})
