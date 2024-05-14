338c581dfeda0894e4c9ffc969d22495895c58c8
tone-man <thetoneman@outlook.com>
2024-05-14 17:05:34 -0400

    Removed src

--- server/tests/page.test.jsx
+++ server/tests/page.test.jsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import Page from '../src/app/page'
+import Page from '../app/page'
  
 describe('Page', () => {
   it('renders hello world', () => {
